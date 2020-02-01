a46b18c54ca83a1ce7aa404ac3813bd11269dec8
HakimPay <hakimpayman@gmail.com>
2020-01-31 23:24:31 -0500

    HP : Did a bit of tests in DAO

--- TP1/enonce/test_DAOs.py
+++ TP1/enonce/test_DAOs.py
@@ -11,18 +11,34 @@ class TestContactDAO(unittest.TestCase):
         self.db_file = 'temp.db'
         self.contactDAO = ContactDAO(self.db_file)
         self.contactDAO.init_db()
-        self.contact1 = Contact(6, "R", "Z", "514999666", "Hakim@gmail.com", False, 20.3)
+        self.contact1 = Contact(6, "R", "Z", "514999666", "Hakim@gmail.com", True, 20.3)
         self.contact2 = Contact(50, "H", "P", "514999666", "Hakim@gmail.com", False, 20.3)
-
-    def compareWithContact1(self, newContact=None):
-        if newContact is not None:
-            assert self.contact1.first_name == newContact.first_name, "should be {}".format(self.contact1.first_name)
-            assert self.contact1.last_name == newContact.last_name, "should be {}".format(self.contact1.last_name)
-            assert self.contact1.phone == newContact.phone, "should be {}".format(self.contact1.phone)
-            assert self.contact1.mail == newContact.mail, "should be {}".format(self.contact1.mail)
-            assert self.contact1.updated == newContact.updated, "should be {}".format(self.contact1.updated)
-            assert self.contact1.updated_date == newContact.updated_date, "should be {}".format(self.contact1.updated_date)
-
+    
+    def printContact(self, contactToPrint):
+        if contactToPrint is None:
+            print("Contact is None. Cannot print")
+        else:
+            print(contactToPrint.first_name)
+            print(contactToPrint.last_name)
+            print(contactToPrint.phone)
+            print(contactToPrint.mail)
+            print(contactToPrint.updated)
+            print(contactToPrint.updated_date)
+            print(contactToPrint)
+    
+    def areContactAttribsEqual(self, contact1 = None, contact2 = None):
+        if contact1 is None:
+            print("First contact is None (see first parameter)")
+            return
+        if contact2 is None:
+            print("Second contact is None (see second parameter)")
+            return
+        self.assertEqual(contact1.first_name, contact2.first_name, "{0} not equal to {1}".format(self.contact1.first_name, self.contact2.first_name))
+        self.assertEqual(contact1.last_name, contact2.last_name, "{0} not equal to {1}".format(self.contact1.last_name, self.contact2.last_name))
+        self.assertEqual(contact1.phone, contact2.phone, "{0} not equal to {1}".format(self.contact1.phone, self.contact2.phone))
+        self.assertEqual(contact1.mail, contact2.mail, "{0} not equal to {1}".format(self.contact1.mail, self.contact2.mail))
+        self.assertEqual(contact1.updated, contact2.updated, "{0} not equal to {1}".format(self.contact1.updated, self.contact2.updated))
+        self.assertEqual(contact1.updated_date, contact2.updated_date, "{0} not equal to {1}".format(self.contact1.updated_date, self.contact2.updated_date))
 
     def tearDown(self):
         os.remove(self.db_file)
@@ -44,35 +60,47 @@ class TestContactDAO(unittest.TestCase):
     
     #On vérifie que contact1 à bel et bien été ajouté
     def test_get_by_id_after_add_should_return_inserted_value(self):
-        newContact = self.contactDAO.get_by_id(self.contactDAO.add(self.contact1))
-        self.compareWithContact1(newContact)
+        self.contactDAO.add(self.contact1)
+        newContact = self.contactDAO.get_by_id(1)
+        self.areContactAttribsEqual(newContact, self.contact1)
     
     def test_get_by_names_after_add_should_return_inserted_value(self):
         self.contactDAO.add(self.contact1)
         newContact = self.contactDAO.get_by_names(self.contact1.first_name, self.contact1.last_name)
-        self.compareWithContact1(newContact)
+        self.areContactAttribsEqual(newContact, self.contact1)
 
     def test_get_by_id_with_undefined_rowid_should_return_None(self):
         self.contactDAO.add(self.contact1)
         newContact = self.contactDAO.get_by_id(2)
-        assert self.compareWithContact1(newContact) == None
+        self.assertIsNone(newContact)
     
     def test_get_by_names_with_notexisted_contact_should_return_None(self):
         self.contactDAO.add(self.contact1)
         newContact = self.contactDAO.get_by_names(self.contact2.first_name, self.contact2.last_name)
-        assert self.compareWithContact1(newContact) == None
+        self.assertIsNone(newContact)
     
+    #Verifier que Contact.updated est assigne a False suite a l'appel de deactivate sur un contact avec updated = True dans la BD
     def test_deactivate_contact_then_get_it_with_id_should_be_not_updated(self):
-        pass
+        ctod = Contact(3, "Bob", "Kump", "514-554-223", "bob.kump@mailmail.com", True, 22.9)
+        self.contactDAO.add(ctod)
+        self.contactDAO.deactivate(1)
+        res = self.contactDAO.get_by_id(1)
+        self.assertFalse(res.updated)
     
     def test_deactivate_contact_on_undefined_id_should_return_zero(self):
-        pass
+        res = self.contactDAO.deactivate(1)
+        self.assertEqual(res, 0, "deactivate did not return 0 on invalid id")
     
     def test_after_deleting_contact_by_id_get_it_with_id_should_return_None(self):
-        pass
+        ctod = Contact(3, "Bob", "Kump", "514-554-223", "bob.kump@mailmail.com", True, 22.9)
+        self.contactDAO.add(ctod)
+        self.contactDAO.delete_by_id(1)
+        res = self.contactDAO.get_by_id(1)
+        self.assertIsNone(res, "getting a deleted contact by id does not return None")
 
     def test_deleting_undefined_id_should_return_zero(self):
-        pass
+        res = self.contactDAO.delete_by_id(1)
+        self.assertEqual(res, 0, "undefined id deletion does not return 0")
 
     def test_after_deleting_contact_by_names_get_item_with_id_should_return_None(self):
         pass
