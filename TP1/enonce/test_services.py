4cd268fdf6841ad311331b02d5d2b8c46d699d75
Roman <Romazho123@gmail.com>
2020-02-11 22:51:36 -0500

    RZ : Ajout des commentaires pour les tests

--- TP1/enonce/test_services.py
+++ TP1/enonce/test_services.py
@@ -7,8 +7,6 @@ from services import AlreadyExistedItem
 from services import UndefinedID
 from services import NotExistedItem
 
-# To complete...
-
 
 class TestContactService(unittest.TestCase):
 
@@ -16,6 +14,7 @@ class TestContactService(unittest.TestCase):
         self.contactDAO = Mock()
         self.contactService = ContactService(self.contactDAO)
 
+    # On test la valeur de l'atribut updated
     def test_when_contact_is_created_updated_should_be_True(self):
         self.contactDAO.add.return_value = 1
         self.contactDAO.get_by_names.return_value = None
@@ -23,6 +22,7 @@ class TestContactService(unittest.TestCase):
             'Houssem', 'Ben Braiek', '123-456-7891', 'houssem.bb@gmail.com')
         self.assertTrue(self.contact.updated)
 
+    # On test la valeur de l'atribut updated_date
     def test_when_contact_is_created_updated_date_should_be_now(self):
         self.contactDAO.add.return_value = 1
         self.contactDAO.get_by_names.return_value = None
@@ -31,11 +31,13 @@ class TestContactService(unittest.TestCase):
         self.assertEqual(int(self.contact.updated_date),
                          int(datetime.now().timestamp()))
 
+    # On test le cas de l'exception AlreadyExistedItem
     def test_when_contact_is_created_and_DAO_get_by_names_returns_contact_it_should_raise_AlreadyExistedItem(self):
         self.contactDAO.add.return_value = 1
         self.assertRaises(AlreadyExistedItem, self.contactService.create_contact,
                           'Houssem', 'Ben Braiek', '123-456-7891', 'houssem.bb@gmail.com')
 
+    # On test la valeur de l'atribut updated
     def test_when_contact_is_changed_updated_should_be_True(self):
         self.contactDAO.add.return_value = 1
         self.contactDAO.get_by_names.return_value = None
@@ -45,45 +47,54 @@ class TestContactService(unittest.TestCase):
             None, 'Houssem', 'Ben Braiek', '123-456-7891', 'lol@gmail.com')
         self.assertTrue(constact.updated)
 
+    # On test la valeur de l'atribut updated_date
     def test_when_contact_is_changed_updated_date_should_be_now(self):
         constact = self.contactService.update_contact(
             None, 'Houssem', 'Ben Braiek', '123-456-7891', 'lol@gmail.com')
         self.assertEqual(int(constact.updated_date),
                          int(datetime.now().timestamp()))
 
+    # On test le cas de l'exception UndefinedID
     def test_when_contact_is_changed_and_DAO_update_returns_zero_it_should_raise_UndefinedID(self):
         self.contactDAO.update.return_value = 0
         self.assertRaises(UndefinedID, self.contactService.update_contact,
                           None, 'Houssem', 'Ben Braiek', '123-456-7891', 'lol@gmail.com')
 
+    # On test le cas de où id n'est pas None
     def test_when_retrieve_contact_is_called_with_id_and_DAO_get_by_id_should_be_called(self):
         self.contactService.retrieve_contact(23, 'Gary', 'Simpson')
         self.contactDAO.get_by_id.assert_called_once_with(id=23)
 
+    # On test le cas de où id est None
     def test_when_retrieve_contact_is_called_with_names_and_DAO_get_by_names_should_be_called(self):
         self.contactService.retrieve_contact(None, 'Gary', 'Simpson')
         self.contactDAO.get_by_names.assert_called_once_with(
             first_name='Gary', last_name='Simpson')
 
+    # On test le cas de l'exception UndefinedID
     def test_when_retrieve_contact_is_called_with_id_and_DAO_returns_None_it_should_raise_UndefinedID(self):
         self.contactDAO.get_by_id.return_value = None
         self.assertRaises(
             UndefinedID, self.contactService.retrieve_contact, 23, 'Gary', 'Simpson')
 
+    # On test le cas de l'exception NotExistedItem
     def test_when_retrieve_contact_is_called_with_names_and_DAO_returns_None_it_should_raise_NotExistedItem(self):
         self.contactDAO.get_by_names.return_value = None
         self.assertRaises(
             NotExistedItem, self.contactService.retrieve_contact, None, 'Gary', 'Simpson')
 
+    # On test le cas de où id n'est pas None
     def test_when_delete_contact_is_called_with_id_and_DAO_delete_by_id_should_be_called(self):
         self.contactService.delete_contact(77, 'Harry', 'Shooter')
         self.contactDAO.delete_by_id.assert_called_once_with(77)
 
+    # On test le cas de où id est None
     def test_when_delete_contact_is_called_with_names_and_DAO_delete_by_names_should_be_called(self):
         self.contactService.delete_contact(None, 'Harry', 'Shooter')
         self.contactDAO.delete_by_names.assert_called_once_with(
             'Harry', 'Shooter')
 
+    # On test le cas de l'exception UndefinedID
     def test_when_delete_contact_is_called_with_id_and_DAO_delete_by_id_returns_zero_it_should_raise_UndefinedID(self):
         self.contactDAO.delete_by_id.return_value = 0
         self.assertRaises(
@@ -96,6 +107,7 @@ class TestContactService(unittest.TestCase):
         self.assertRaises(
             NotExistedItem, self.contactService.delete_contact, None, 'oki', 'doki')
 
+    # On test le cas où le delta.days est plus grand que 1095
     def test_when_verify_contacts_status_is_called_and_delta_days_is_bigger_than_1095_it_should_call_DAO_deactivate(self):
         contact = Contact(1, 'first_name', 'last_name', 'phone',
                           'mail', True, datetime.now().timestamp() - 990000000)
@@ -103,6 +115,7 @@ class TestContactService(unittest.TestCase):
         self.contactService.verify_contacts_status()
         self.contactDAO.deactivate.assert_called()
 
+    # On test le cas où le delta.days est plus petit que 1095
     def test_when_verify_contacts_status_is_called_and_contact_is_new_it_should_not_call_DAO_deactivate(self):
         contact = Contact(1, 'first_name', 'last_name', 'phone',
                           'mail', True, datetime.now().timestamp())
@@ -110,12 +123,14 @@ class TestContactService(unittest.TestCase):
         self.contactService.verify_contacts_status()
         self.contactDAO.assert_not_called()
 
+    # On test les cas où le mail est de bon format
     def test_when_check_mail_is_called_with_correct_mail_it_should_return_true(self):
         self.assertTrue(self.contactService.check_mail("dude123@gmail.com"))
         self.assertTrue(self.contactService.check_mail("123@321.org"))
         self.assertTrue(self.contactService.check_mail(
             "this.is.a.legit.email.so.dont.email.me.thanks@gmail.lol"))
 
+    # On test les cas où le mail est de mauvais format
     def test_when_check_mail_is_called_with_incorrect_mail_it_should_return_false(self):
         self.assertFalse(self.contactService.check_mail("not an Email lol"))
         self.assertFalse(self.contactService.check_mail("close@butNotEmail"))
@@ -125,11 +140,13 @@ class TestContactService(unittest.TestCase):
         self.assertFalse(self.contactService.check_mail("@dont.com"))
         self.assertFalse(self.contactService.check_mail("mail.me@.com"))
 
+    # On test les cas où le numéro de téléphone est de bon format
     def test_when_check_phone_is_called_with_correct_number_it_should_return_true(self):
         self.assertTrue(self.contactService.check_phone("123-321-7777"))
         self.assertTrue(self.contactService.check_phone("999-999-9999"))
         self.assertTrue(self.contactService.check_phone("000-000-0000"))
 
+    # On test les cas où le numéro de téléphone est de mauvais format
     def test_when_check_phone_is_called_with_incorrect_number_it_should_return_false(self):
         self.assertFalse(self.contactService.check_phone(""))
         self.assertFalse(self.contactService.check_phone("12 leter str"))
