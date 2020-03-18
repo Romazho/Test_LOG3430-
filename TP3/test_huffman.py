9a7fc3e1b85a3bea338ab789f0801177aaed58e7
HakimPay <hakimpayman@gmail.com>
2020-03-18 16:21:43 -0400

    HP : WIP icant live

--- TP3/test_huffman.py
+++ TP3/test_huffman.py
@@ -5,46 +5,66 @@ import unittest
 import heapq
 
 
-class TestBST(unittest.TestCase):
+class TestHuffman(unittest.TestCase):
+    ########## Tests de la tranche weight ##########
 
-    def setUp(self):
-        self.huffman = Huffman(0, 'testing')
+    # On test l'initialisation de l'attribut weight avec le constructeur de la classe Huffman
+    def test_init_it_should_initialize_weight_with_parameter(self):
+        huffman = Huffman(1, 'abc')
+        self.assertEqual(huffman.weight, 1)
+    
+    # On test la valeur de l'attribut weight suite à un appel à la fonction from_string
+    def test_from_string_it_should_assign_length_of_string_to_weight_of_heap_head(self):
+        huffman = Huffman.from_string('abbcccddddeeeee')
+        self.assertEqual(huffman.weight, len('abbcccddddeeeee'))
+    
+    ########## Tests de la tranche data ##########
 
-    ################ tests pour l'attribut weight ################
+    # On test l'initialisation de l'attribut data avec le constructeur de la classe Huffman
+    def test_init_it_should_initialize_data_with_parameter(self):
+        huffman = Huffman(1, 'abc')
+        self.assertEqual(huffman.data, 'abc')
+    
+    # On test la valeur de l'attribut data suite à un appel à la fonction from_string
+    def test_from_string_it_should_assign_data_with_parameter(self):
+        huffman = Huffman.from_string('cab')
+        self.assertEqual(huffman.data, 'cab')
+    
+    # On test les valeurs indexées par data dans le codebook suite à un appel à la méthode build_codebook
+    def test_build_codebook_it_should_correctly_assign_leaf_prefixes_according_to_Huffman_algorithm(self):
+        left = Huffman(1, 'a')
+        right = Huffman(1, 'b')
+        huffman = Huffman(2, 'ab', left, right)
+        self.assertEqual(huffman.codebook[huffman.left.data], '0')
+        self.assertEqual(huffman.codebook[huffman.right.data], '1')
+    
+    ########## Tests de la tranche left ##########
 
-    # On vérifie si l'initialisation de la variable weight s'est assigné correctement
-    def test_init_it_should_initialize_weight(self):
-        self.assertEqual(self.huffman.weight, 0)
-        self.assertEqual(self.huffman.left, None)  # à changer de place
-        self.assertEqual(self.huffman.right, None)  # à changer de place
-        self.assertEqual(self.huffman.codebook, {})  # à changer de place
+    # On test la valeur de l'attribut left avec le constructeur de la classe Huffman
+    def test_init_it_should_initialize_left_with_parameter(self):
+        left = Huffman(2, 'def')
+        huffmanWithLeft = Huffman(1, 'abc', left)
+        huffmanNoLeft = Huffman(1, 'abc')
+        self.assertEqual(huffmanWithLeft.left, left)
+        self.assertIsNone(huffmanNoLeft.left)
+    
 
-    # On vérifie si l'asignation de l'attribut de weight s'est asiigné corrctement
-    def test_from_string_it_should_assign_weight_according_to_huffman_algorithm(self):
-        huff2 = Huffman.from_string("testing")
-        self.assertEqual(huff2.weight, len("testing"))
+    ########## Tests de la tranche right ##########
 
-    ################ tests pour l'attribut data ################
+    # On test la valeur de l'attribut right avec le constructeur de la classe Huffman
+    def test_init_it_should_initialize_right_with_parameter(self):
+        right = Huffman(2, 'def')
+        huffmanWithRight = Huffman(1, 'abc', None, right)
+        huffmanNoRight = Huffman(1, 'abc')
+        self.assertEqual(huffmanWithRight.right, right)
+        self.assertIsNone(huffmanNoRight.right)
+    
+    ########## Tests de la tranche codebook ##########
 
-    # On vérifie si l'initialisation de la variable data s'est assigné correctement
-    def test_init_it_should_initialize_data(self):
-        self.assertEqual(self.huffman.data, "testing")
-
-    # On vérifie que le data n'a pas été changé
-    def test_build_codebook_it_should_not_change_data(self):
-        self.huffman.build_codebook()
-        self.assertEqual(self.huffman.data, "testing")
-
-    # On vérifie que le data est bien initialisé au string passé en argument
-    def test_from_string_it_should_assign_data_according_to_huffman_algorithm(self):
-        Huffman.from_string("testing")
-        self.assertEqual(self.huffman.data, "testing")
-
-    # I have no idea what I am doing
-    def test_encode_tree_it_should_(self):
-        leaves = []
-        self.assertEqual(self.huffman.encode_tree(), ('1', "testing"))
-
-
+    # On test la valeur de l'attribut codebook avec le constructeur de la classe Huffman
+    def test_init_it_should_initialize_codebook(self):
+        huffman = Huffman(1, 'abc')
+        self.assertEqual(huffman.codebook, {})
+        
 if __name__ == '__main__':
     unittest.main()
